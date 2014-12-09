e4403d84a07ab4be77c3ba1a4ecaef0b4767a895
Stephen Mathieson <smath23@gmail.com>
2014-12-09 11:46:01 -0500

    repo: Change default command to `clone` or `cd`
    Based on whether or not the repo has already been cloned (has a `.git`
    dir), we'll either clone it or cd to it.
    
    Closes #1.

--- repo.sh
+++ repo.sh
@@ -7,17 +7,47 @@
 REPO_ROOT=${REPO_ROOT-~/repos}
 
 #
+# parse the given slug ($2), then callback the given fn ($1)
+#
+
+_repo_parse(){
+  local fn=$1
+  local slug="$2"
+  local parts
+  # parse slug into provider, user & repo
+  IFS='/' read -a parts <<< "$slug"
+  local len=${#parts[@]}
+  local provider="github.com"
+  local user=""
+  local repo=""
+  if [[ $len -eq 3 ]]; then
+    provider="${parts[0]}"
+    user="${parts[1]}"
+    repo="${parts[2]}"
+  elif [[ $len -eq 2 ]]; then
+    user="${parts[0]}"
+    repo="${parts[1]}"
+  else
+    echo "invalid repo slug" >&2
+    return 1
+  fi
+
+  # callback
+  $fn $provider $user $repo
+}
+
+#
 # output usage
 #
 
-_repo_usage() {
+_repo_usage(){
   echo ""
   echo "  repo [command] <slug>"
   echo ""
   echo "  Supported commands:"
   echo ""
   echo "    help    Show this help text"
-  echo "    cd      Literally 'cd <slug>' (default command)"
+  echo "    cd      Literally 'cd <slug>'"
   echo "    clone   Clone the new repo, then cd to it"
   echo "    rm      Remove the old repo"
   echo ""
@@ -30,7 +60,7 @@ _repo_usage() {
   echo ""
   echo "  Examples:"
   echo ""
-  echo "    # cd to somebody/somerepo"
+  echo "    # clone and/or cd to somebody/somerepo"
   echo "    $ repo somebody/somerepo"
   echo ""
   echo "    # clone somebody/somerepo"
@@ -42,10 +72,10 @@ _repo_usage() {
 }
 
 #
-# cd to the the repo
+# cd to the given repo (composed of $1/$2/$3)
 #
 
-_repo_cd() {
+_repo_cd(){
   # exit on error
   set +e
 
@@ -57,10 +87,10 @@ _repo_cd() {
 }
 
 #
-# clone the given the repo, then cd to it
+# clone the given the repo ($1/$2/$3), then cd to it
 #
 
-_repo_clone() {
+_repo_clone(){
   # exit on error
   set +e
 
@@ -79,10 +109,10 @@ _repo_clone() {
 }
 
 #
-# rm -rf the repo
+# rm -rf the repo (composed of $1/$2/$3)
 #
 
-_repo_rimraf() {
+_repo_rimraf(){
   # exit on error
   set +e
   local dir="${REPO_ROOT}/$1/$2/$3"
@@ -90,49 +120,25 @@ _repo_rimraf() {
 }
 
 #
-# main
+# process the command
 #
 
-repo() {
-  local cmd="$1"
-  local slug="$2"
+_repo_arbiter(){
+  local provider="$1"
+  local user="$2"
+  local repo="$3"
 
-  # help
-  if [[ "$cmd" = "help" ]] || [[ "$cmd" = "--help" ]] ; then
-    _repo_usage
-    return 0
-  fi
-
-  # validate args
-  if [[ -z "$cmd" ]]; then
-    echo "repository slug required" >&2
-    return 1
-  elif [[ -z "$slug" ]]; then
-    # optional command
-    slug="${cmd}"
-    cmd="cd"
-  fi
-
-  # parse slug into provider, user & repo
-  IFS='/' read -a parts <<< "$slug"
-  len=${#parts[@]}
-  provider="github.com"
-  user=""
-  repo=""
-  if [[ $len -eq 3 ]]; then
-    provider="${parts[0]}"
-    user="${parts[1]}"
-    repo="${parts[2]}"
-  elif [[ $len -eq 2 ]]; then
-    user="${parts[0]}"
-    repo="${parts[1]}"
-  else
-    echo "invalid repo slug" >&2
-    return 1
-  fi
-
-  # run cmd
+  # find command
   case $cmd in
+    "default")
+      local git="${REPO_ROOT}/${provider}/${user}/${repo}/.git"
+      if [[ -d "${git}" ]]; then
+        _repo_cd $provider $user $repo
+      else
+        _repo_clone $provider $user $repo
+      fi
+      return $?
+      ;;
     "cd")
       _repo_cd $provider $user $repo
       return $?
@@ -153,4 +159,32 @@ repo() {
       return 1
       ;;
   esac
+}
+
+#
+# main
+#
+
+repo(){
+  local cmd="$1"
+  local slug="$2"
+
+  # help
+  if [[ "$cmd" = "help" ]] || [[ "$cmd" = "--help" ]] ; then
+    _repo_usage
+    return 0
+  fi
+
+  # validate args
+  if [[ -z "$cmd" ]]; then
+    echo "repository slug required" >&2
+    return 1
+  elif [[ -z "$slug" ]]; then
+    # optional command
+    slug="${cmd}"
+    cmd="default"
+  fi
+
+  # parse slug, then invoke arbiter
+  _repo_parse _repo_arbiter "$slug" "$cmd"
 }
